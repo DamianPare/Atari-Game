f3e26bbf8ca813342ddc278a2c280667c37aa4a0
Miriam22131 <miriamayach@gmail.com>
2024-09-23 21:14:42 -0700

    Created Game manager
    created code for game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject startScreen; // Assign in Inspector
+    public GameObject gameScreen; // Assign in Inspector
+    public Text fuelText; // Assign in Inspector
+    public float maxFuel = 100f;
+
+    private float currentFuel;
+    public PlayerMovement playerController; // Reference to the player controller
+
+    void Start()
+    {
+        currentFuel = maxFuel;
+        ShowStartScreen();
+        playerController.enabled = false; // Disable player movement at start
+    }
+
+    void Update()
+    {
+        if (startScreen.activeSelf)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                StartGame();
+            }
+        }
+        else if (gameScreen.activeSelf)
+        {
+            currentFuel -= Time.deltaTime; // Decrease fuel over time
+            UpdateFuelUI();
+
+            if (currentFuel <= 0)
+            {
+                EndGame();
+            }
+        }
+    }
+
+    void StartGame()
+    {
+        startScreen.SetActive(false);
+        gameScreen.SetActive(true);
+        currentFuel = maxFuel; // Reset fuel for new game
+        playerController.enabled = true; // Enable player movement
+    }
+
+    void EndGame()
+    {
+        gameScreen.SetActive(false);
+        ShowStartScreen();
+        playerController.enabled = false; // Disable player movement on game over
+    }
+
+    void ShowStartScreen()
+    {
+        startScreen.SetActive(true);
+        gameScreen.SetActive(false);
+        UpdateFuelUI();
+    }
+
+    void UpdateFuelUI()
+    {
+        fuelText.text = "Fuel: " + Mathf.Max(currentFuel, 0).ToString("F2");
+    }
+}
