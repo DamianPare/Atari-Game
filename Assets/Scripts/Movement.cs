43e59b3181e8fd28e6074c48fd934d9aad10084f
LuisVivancoG <48226613+LuisVivancoG@users.noreply.github.com>
2024-09-23 23:34:55 -0700

    Movement animations and settings for WebGL
    Referencing animations during movement and changing settings to run web exe

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f; // Speed of the player
     private Rigidbody2D rb; // Reference to the Rigidbody2D component
     private Vector2 movement; // Store movement input
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer spriteRenderer;
 
     private void Start()
     {
@@ -18,11 +20,37 @@ public class PlayerMovement : MonoBehaviour
         // Reset movement vector
         movement = Vector2.zero;
 
+        if (movement.y == 0 && movement.x == 0)
+        {
+            animator.SetBool("isMoving?", false);
+        }
+
         // Get input from keyboard for WASD keys
-        if (Input.GetKey(KeyCode.W)) movement.y = 1f; // Move up
-        if (Input.GetKey(KeyCode.S)) movement.y = -1f; // Move down
-        if (Input.GetKey(KeyCode.A)) movement.x = -1f; // Move left
-        if (Input.GetKey(KeyCode.D)) movement.x = 1f; // Move right
+        if (Input.GetKey(KeyCode.W))  // Move up
+        {
+            animator.SetBool("isMoving?", true);
+            movement.y = 1f;
+        }
+
+        if (Input.GetKey(KeyCode.S))  // Move down
+        {
+            animator.SetBool("isMoving?", true);
+            movement.y = -1f;
+        }
+
+        if (Input.GetKey(KeyCode.A))  // Move left
+        {
+            animator.SetBool("isMoving?", true);
+            spriteRenderer.flipX = true;
+            movement.x = -1f;
+        }
+        
+        if (Input.GetKey(KeyCode.D))  // Move right
+        {
+            animator.SetBool("isMoving?", true);
+            spriteRenderer.flipX = false;
+            movement.x = 1f;
+        }   
     }
 
     private void FixedUpdate()
